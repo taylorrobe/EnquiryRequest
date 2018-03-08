bc5f108feb0c0a89685ed5806e51cdb3096b45c9
taylorrobe <rob@lolapearl.co.uk>
2018-03-08 15:20:41 +0000

    Add project files.

---
+++ EnquiryRequest3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnquiryRequest3.Startup))]
+namespace EnquiryRequest3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
