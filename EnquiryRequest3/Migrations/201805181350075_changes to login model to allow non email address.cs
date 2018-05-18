eb1d37fc75c3c0d9e5843f5d466fa298d959b6ea
taylorrobe <rob@lolapearl.co.uk>
2018-05-18 15:45:45 +0100

    solved problem with selecting polygons

---
+++ EnquiryRequest3/Migrations/201805181350075_changes to login model to allow non email address.cs
@@ -0,0 +1,16 @@
+namespace EnquiryRequest3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changestologinmodeltoallownonemailaddress : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
