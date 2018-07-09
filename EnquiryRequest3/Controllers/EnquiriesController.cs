b06651ede4f5e46ec6b31a794480dfe5a20d6764
taylorrobe <rob@lolapearl.co.uk>
2018-07-09 12:05:17 +0100

    back

--- EnquiryRequest3/Controllers/EnquiriesController.cs
+++ EnquiryRequest3/Controllers/EnquiriesController.cs
@@ -195,7 +195,6 @@ namespace EnquiryRequest3.Controllers
             ViewBag.Boundaries = spatial.GetGeoJsonCollectionFromBoundaryCollection(db.Boundaries.ToList(), SpatialHelper.BoundaryType.DISPLAY);
             ViewBag.Coverage = spatial.GetGeoJsonCollectionFromBoundaryCollection(db.Boundaries.ToList(), SpatialHelper.BoundaryType.COVERAGE);
             return View(newEnquiry);
-            //test comment
         }
 
         // POST: Enquiries/Create
